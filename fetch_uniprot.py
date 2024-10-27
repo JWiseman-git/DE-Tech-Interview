3bfd1bb1edc9227c3b32edcb0ca54eb5bea884d6
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2024-10-27 20:10:11 +0000

    Update fetch_uniprot.py

--- fetch_uniprot.py
+++ fetch_uniprot.py
@@ -1,2 +1,11 @@
 import urllib.request
-urllib.request.urlretrieve('https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml', 'Uniprot.xml')
+# urllib.request.urlretrieve('https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml', 'Uniprot.xml')
+
+def report_hook(block_num, block_size, total_size):
+    downloaded = block_num * block_size
+    percentage = downloaded / total_size * 100
+    print(f'Downloaded {downloaded} of {total_size} bytes ({percentage:.2f}%)', end='\r')
+
+# Retrieve the file with progress reporting
+urllib.request.urlretrieve('https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml', 'Uniprot.xml', reporthook=report_hook)
+print() 
