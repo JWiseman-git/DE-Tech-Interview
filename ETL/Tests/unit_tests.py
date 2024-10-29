a0c31d1b847e1774a8380eb5b45cf12ef780d380
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2024-10-29 07:56:41 +0000

    Update unit_tests.py

--- ETL/Tests/unit_tests.py
+++ ETL/Tests/unit_tests.py
@@ -27,6 +27,7 @@ class TestDataFrameOutput(unittest.TestCase):
         result_df = download_and_filter_parquet_files(self.server, self.url)
 
         self.assertFalse(result_df.empty, "The DataFrame is empty")
+        print('Test Passed: Dataframe was not empty')
     
 
     def test_return_dataframe_fields(self):
@@ -36,6 +37,8 @@ class TestDataFrameOutput(unittest.TestCase):
         result_df = download_and_filter_parquet_files(self.server, self.url, self.expected_fields)
 
         self.assertTrue(self.expected_fields.issubset(result_df.columns), "Missing expected fields in DataFrame")
+        
+        print('Test Passed: All expected fiedls were found in Dataframe')
 
 if __name__ == '__main__':
     unittest.main()
